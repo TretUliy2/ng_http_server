2f5d25467b696c36afcb67472cd75ae030f2bfdf
TretUliy2 <tretuliy2@gmail.com>
2015-09-13 15:13:04 +0300

    a lot of changes

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 default:
-	cc -c main.c
-	cc -lc -lnetgraph -o http_server *.o
+	gcc5 -c main.c
+	gcc5 -lc -lnetgraph -o http_server *.o
 clean:
 	rm -rf *.o *.core http_server
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sysexits.h>
 #include <errno.h>
 #include <string.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netgraph.h>
+#include <sys/socket.h>
 #include <netgraph/ng_ksocket.h>
 
 #define SRC_IP "0.0.0.0"
@@ -14,7 +17,8 @@ static int csock, dsock;
 
 int CreateNgSock(void);
 int StartServer(void);
-
+int ListenNgKsocket();
+int CreateNgKsocket();
 
 int main( int argc, char *argv[] ) {
     printf("Starting http server...\n");
@@ -40,6 +44,7 @@ int StartServer() {
 
 int CreateNgKsocket() {
     char path[NG_PATHSIZ];
+    char ksock_name[NG_NODESIZ];
     struct ngm_mkpeer arg;
     
     sprintf(arg.type, "ksocket");
@@ -52,6 +57,13 @@ int CreateNgKsocket() {
         printf("%s(): Error sending message: %s", __func__, strerror(errno));
         return -1;
     }
+    sprintf(path, ".:%s", arg.ourhook);
+    memset( ksock_name, 0, sizeof(ksock_name) );
+    sprintf(ksock_name, "http-ksocket-%d", getpid());
+    if ( NgNameNode(csock, path, ksock_name, getpid()) < 0 ) {
+        printf("%s: error naming node : %s", __func__, strerror(errno));
+        return -1;
+    }
 
     /* make kscoket bind and listen state */
     struct sockaddr_in source;
@@ -59,18 +71,39 @@ int CreateNgKsocket() {
     inet_aton(SRC_IP, &source.sin_addr);
     source.sin_port = htons(atoi(SRC_PORT));
     source.sin_len = sizeof(struct sockaddr_in);
+    sprintf(path, "%s:", ksock_name);
     NgSetDebug(3);
     if ( NgSendMsg(csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_BIND,  &source, sizeof(source)) < 0 ) {
         printf("%s(): Error sending ksocket bind: %s", __func__, strerror(errno));
         return -1;
     }
-
-
+    /* Listening ksocket */
+    int lst = 1024;
+    if ( NgSendMsg(csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_LISTEN,  &lst, sizeof(lst)) < 0 ) {
+        printf("%s(): Error sending ksocket bind: %s", __func__, strerror(errno));
+        return -1;
+    }
+    int token;
+    token = NgSendMsg(srv_csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_ACCEPT, NULL, 0);
+    if ( token < 0 && errno != EINPROGRESS && errno != EALREADY ) {
+        printf("%s(): Accept Failed %s", __func__, strerror(errno));
+        return -1;
+    }
+    return token;
 }
 
 int ListenNgKsocket() {
+    struct ng_mesg *m;
+    char path[NG_PATHSIZ];
+    int token;
+    memset(path, 0, sizeof(path));
     
     for (;;) {
-           
+        if ( NgAllocRecvMsg(csock, &m, path) < 0 ) {
+            printf("%s(): Error receiving message : %s", __func__, strerror(errno));
+            return -1;
+        }
+        printf("new client nod = %s", path);
+        
     }
 }
