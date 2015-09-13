7e7f2afe12a4aaf35730f54d6fd13221b2b7dadb
TretUliy2 <tretuliy2@gmail.com>
2015-09-13 22:43:13 +0300

    a lot of changes

--- main.c
+++ main.c
@@ -14,11 +14,15 @@
 #define SRC_PORT "8000"
 
 static int csock, dsock;
+static char ksock_name[NG_NODESIZ];
 
 int CreateNgSock(void);
 int StartServer(void);
 int ListenNgKsocket();
 int CreateNgKsocket();
+int sendAccept(char path[NG_PATHSIZ]);
+int SetKsocketKeepAlive (char path[NG_PATHSIZ]);
+
 
 int main( int argc, char *argv[] ) {
     printf("Starting http server...\n");
@@ -38,13 +42,15 @@ int CreateNgSock() {
 }
 
 int StartServer() {
-    CreateNgKsocket();
+    if ( CreateNgKsocket() < 0 ) {
+        return -1;
+    }
+
     ListenNgKsocket();
 }
 
 int CreateNgKsocket() {
     char path[NG_PATHSIZ];
-    char ksock_name[NG_NODESIZ];
     struct ngm_mkpeer arg;
     
     sprintf(arg.type, "ksocket");
@@ -74,36 +80,89 @@ int CreateNgKsocket() {
     sprintf(path, "%s:", ksock_name);
     NgSetDebug(3);
     if ( NgSendMsg(csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_BIND,  &source, sizeof(source)) < 0 ) {
-        printf("%s(): Error sending ksocket bind: %s", __func__, strerror(errno));
+        printf("%s(): Error sending ksocket bind: %s\n", __func__, strerror(errno));
         return -1;
     }
     /* Listening ksocket */
     int lst = 1024;
     if ( NgSendMsg(csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_LISTEN,  &lst, sizeof(lst)) < 0 ) {
-        printf("%s(): Error sending ksocket bind: %s", __func__, strerror(errno));
+        printf("%s(): Error sending ksocket bind: %s\n", __func__, strerror(errno));
         return -1;
     }
     int token;
-    token = NgSendMsg(srv_csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_ACCEPT, NULL, 0);
+    token = NgSendMsg(csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_ACCEPT, NULL, 0);
     if ( token < 0 && errno != EINPROGRESS && errno != EALREADY ) {
-        printf("%s(): Accept Failed %s", __func__, strerror(errno));
+        printf("%s(): Accept Failed %s\n", __func__, strerror(errno));
         return -1;
     }
+    SetKsocketKeepAlive(ksock_name);
     return token;
 }
 
 int ListenNgKsocket() {
-    struct ng_mesg *m;
     char path[NG_PATHSIZ];
     int token;
-    memset(path, 0, sizeof(path));
     
+    struct ng_mesg *m;
+    struct accept_response {
+        int node_id;
+        struct sockaddr_in client;
+    } *resp;
+    
+    memset(path, 0, sizeof(path));
+    printf("%s serving connections\n", __func__); 
     for (;;) {
         if ( NgAllocRecvMsg(csock, &m, path) < 0 ) {
-            printf("%s(): Error receiving message : %s", __func__, strerror(errno));
+            printf("%s(): Error receiving message : %s\n", __func__, strerror(errno));
             return -1;
         }
-        printf("new client nod = %s", path);
-        
+        resp = (struct accept_response *)m->data;
+        printf("new client nod = %s token = %d arglen = %d\n", path, m->header.token, m->header.arglen);
+        printf("client_node_id = %08X client_address = %s:%d\n", resp->node_id, 
+                inet_ntoa(resp->client.sin_addr), 
+                htons(resp->client.sin_port)
+        );
+        sendAccept(ksock_name);
+        free(m);
     }
 }
+
+int sendAccept(char path[NG_PATHSIZ]) {
+    uint32_t token;
+    if ( path[strlen(path) - 1] != ':' ) 
+        sprintf(path, "%s:", path);
+    token = NgSendMsg(csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_ACCEPT,
+    NULL, 0);
+    if ((int) token < 0 && errno != EINPROGRESS && errno != EALREADY) {
+        printf("%s(): Error has occured : %s\n" ,__func__, strerror(errno));
+    }
+    return token;
+}
+
+int SetKsocketKeepAlive (char path[NG_PATHSIZ]) {
+	union {
+		u_char buf[sizeof(struct ng_ksocket_sockopt) + sizeof(int)];
+		struct ng_ksocket_sockopt sockopt;
+	} sockopt_buf;
+	struct ng_ksocket_sockopt * const sockopt = &sockopt_buf.sockopt;
+	int one = 1;
+
+    if ( path[strlen(path) - 1] != ':' ) 
+        sprintf(path, "%s:", path);
+	// setsockopt resolve TIME_WAIT problem
+	// setsockopt(fd,SOL_SOCKET,SO_REUSEPORT,&one,sizeof(int)) < 0)
+	memset(&sockopt_buf, 0, sizeof(sockopt_buf));
+
+	sockopt->level = SOL_SOCKET;
+	sockopt->name = SO_KEEPALIVE;
+	memcpy(sockopt->value, &one, sizeof(int));
+	if (NgSendMsg(csock, path, NGM_KSOCKET_COOKIE, NGM_KSOCKET_SETOPT,
+			sockopt, sizeof(sockopt_buf)) == -1) {
+		fprintf(stderr, "%s(): Sockopt set failed : %s\n", __FUNCTION__,
+				strerror(errno));
+		return -1;
+	}
+	return 1;
+}
+
+
